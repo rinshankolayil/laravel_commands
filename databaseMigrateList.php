d5b13e15165bf0fdb1ad069575abf306492afc79
Rinshan Kolayil <40558052+rinshankolayil@users.noreply.github.com>
2020-11-07 12:43:36 +0300

    Update databaseMigrateList.php

--- databaseMigrateList.php
+++ databaseMigrateList.php
@@ -15,14 +15,14 @@ class DbMigrateList
     {
         $message = 'Are you sure wish to continue? ' . "\r\n";
         $message .= ' 1 - Please make sure that all database have added into ' . __DIR__ . '\\databaseMigrateList.php ' . "\r\n";
-        $message .= ' 2 - Please make sure to pass argument `--common_db=db_auth` for the database `db_common`' . "\r\n";
+        $message .= ' 2 - Please make sure to pass argument `--common_db=common_db` for the database `common_db`' . "\r\n";
         return  $message;
     }
     static public function ConfirmMigrationMessage()
     {
         //PATH =>C:\wamp64\www\laravel_test\blog\vendor\laravel\framework\src\Illuminate\Database\Console\Migrations
         $message = 'Are you sure wish to continue?. ' . "\r\n";
-        $message .= ' Please make sure to pass argument `--path=/database/migrations/db_common` for the database `db_common` ' . "\r\n";
+        $message .= ' Please make sure to pass argument `--path=/database/migrations/common_db` for the database `db_common` ' . "\r\n";
         return  $message;
     }
 }
