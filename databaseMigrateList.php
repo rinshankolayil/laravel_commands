9286134fd5e16e273b98f64e8c1943cf26226a28
Rinshan Kolayil <40558052+rinshankolayil@users.noreply.github.com>
2020-11-09 10:44:22 +0300

    Update databaseMigrateList.php

--- databaseMigrateList.php
+++ databaseMigrateList.php
@@ -1,4 +1,15 @@
-static public function getDefaultDbPath($retun_folder_name = '')
+<?php
+
+namespace App\Console\Commands;
+
+use App\Libraries\ASJ;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+
+class DbMigrateList
+{
+
+    static public function getDefaultDbPath($retun_folder_name = '')
     {
         $folder_name = 'common_db_migrations';
         if ($retun_folder_name != "") {
@@ -29,3 +40,42 @@ static public function getDefaultDbPath($retun_folder_name = '')
         }
         return $dbNames;
     }
+
+    static public function ConfirmationMessage()
+    {
+        $message = 'Are you sure wish to continue? ' . "\r\n";
+        $message .= ' 1 - Please make sure that all database have added into ' . __DIR__ . '\\databaseMigrateList.php ' . "\r\n";
+        $message .= ' 2 - Please make sure to pass argument `--common_db=' . self::getDefaultDbPath('folder_name') . '` for the database `' . self::getDefaultDbPath('folder_name') . '`' . "\r\n";
+        return  $message;
+    }
+    static public function ConfirmMigrationMessage()
+    {
+        //PATH =>C:\wamp64\www\laravel_test\blog\vendor\laravel\framework\src\Illuminate\Database\Console\Migrations
+        $message = 'Are you sure wish to continue?. ' . "\r\n";
+        $message .= ' Please make sure to pass argument `--path=/database/migrations/' . self::getDefaultDbPath('folder_name') . '` for the database `' . self::getDefaultDbPath('folder_name') . '` ' . "\r\n";
+        return  $message;
+    }
+
+    static public function ConfirmRollBackApproved()
+    {
+        $sql = "SELECT * FROM rollback_approval WHERE status='A'";
+        $result = DB::connection(self::getDBNames('default'))->select($sql);
+        ASJ::PRE($result);
+    }
+
+    static public function renameMigratedFile($path, $file)
+    {
+        $full_path = ltrim($path, "/") . '/' . $file . '.php';
+        if (file_exists($full_path)) {
+            $dbName = self::getDBNames('default');
+            $file_content = file_get_contents($full_path);
+            $file_replaced = str_replace("Schema::create(", "Schema::connection('" . $dbName . "')->create(", $file_content);
+            $file_replaced = str_replace("Schema::dropIfExists(", "Schema::connection('" . $dbName . "')->dropIfExists(", $file_replaced);
+            $down_replace = "//PLEASE MAKE SURE TO VERIFY `Schema::connection('" . $dbName . "')`\r\n\tpublic function down()";
+            $up_replace = "//PLEASE MAKE SURE TO VERIFY `Schema::connection('" . $dbName . "')`\r\n\tpublic function up()";
+            $file_replaced = str_replace("public function down()", $down_replace, $file_replaced);
+            $file_replaced = str_replace("public function up()", $up_replace, $file_replaced);
+            file_put_contents($full_path, $file_replaced);
+        }
+    }
+}
