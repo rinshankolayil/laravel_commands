29b94e6f4538c095f996a59ec92b318e059fb13a
Rinshan Kolayil <40558052+rinshankolayil@users.noreply.github.com>
2020-11-09 09:54:45 +0300

    Update databaseMigrateList.php
    New update

--- databaseMigrateList.php
+++ databaseMigrateList.php
@@ -1,13 +1,29 @@
 <?php
 
 namespace App\Console\Commands;
+use Illuminate\Support\Facades\DB;
 
 class DbMigrateList
 {
 
-    static public function getDBNames()
+    static public function getDefaultDbPath($retun_folder_name = '')
+    {
+        $folder_name = 'common_db_path';
+        if ($retun_folder_name != "") {
+            return $folder_name;
+        }
+        return array(
+            'folderName' => $folder_name,
+            'fullPath' => '/database/migrations/' . $folder_name,
+        );
+    }
+
+    static public function getDBNames($common_db = '')
     {
         $dbNames = ['db_test_1', 'db_test_2'];
+        if ($common_db == 'default') {
+            return 'common_db'; // COMMON DB
+        }
         return $dbNames;
     }
 
@@ -15,14 +31,37 @@ class DbMigrateList
     {
         $message = 'Are you sure wish to continue? ' . "\r\n";
         $message .= ' 1 - Please make sure that all database have added into ' . __DIR__ . '\\databaseMigrateList.php ' . "\r\n";
-        $message .= ' 2 - Please make sure to pass argument `--common_db=common_db` for the database `common_db`' . "\r\n";
+        $message .= ' 2 - Please make sure to pass argument `--common_db=' . self::getDefaultDbPath('folder_name') . '` for the database `' . self::getDefaultDbPath('folder_name') . '`' . "\r\n";
         return  $message;
     }
     static public function ConfirmMigrationMessage()
     {
         //PATH =>C:\wamp64\www\laravel_test\blog\vendor\laravel\framework\src\Illuminate\Database\Console\Migrations
         $message = 'Are you sure wish to continue?. ' . "\r\n";
-        $message .= ' Please make sure to pass argument `--path=/database/migrations/common_db` for the database `common_db` ' . "\r\n";
+        $message .= ' Please make sure to pass argument `--path=/database/migrations/' . self::getDefaultDbPath('folder_name') . '` for the database `' . self::getDefaultDbPath('folder_name') . '` ' . "\r\n";
         return  $message;
+    }
+
+    static public function ConfirmRollBackApproved()
+    {
+        $sql = "SELECT * FROM rollback_approval WHERE status='A'";
+        $result = DB::connection(self::getDBNames('default'))->select($sql);
+        return $result;
+    }
+
+    static public function renameMigratedFile($path, $file)
+    {
+        $full_path = ltrim($path, "/") . '/' . $file . '.php';
+        if (file_exists($full_path)) {
+            $dbName = self::getDBNames('default');
+            $file_content = file_get_contents($full_path);
+            $file_replaced = str_replace("Schema::create(", "Schema::connection('" . $dbName . "')->create(", $file_content);
+            $file_replaced = str_replace("Schema::dropIfExists(", "Schema::connection('" . $dbName . "')->dropIfExists(", $file_replaced);
+            $down_function = "//PLEASE MAKE SURE TO VERIFY `Schema::connection('" . $dbName . "')`\r\n\tpublic function down()";
+            $up_function = "//PLEASE MAKE SURE TO VERIFY `Schema::connection('" . $dbName . "')`\r\n\tpublic function up()";
+            $file_replaced = str_replace("public function down()", $down_function, $file_replaced);
+            $file_replaced = str_replace("public function up()", $up_function, $file_replaced);
+            file_put_contents($full_path, $file_replaced);
+        }
     }
 }
